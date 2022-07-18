53e50cdf1a3830a123dce12ae2edcc3cb15f162d
siva50 <80094666+siva50@users.noreply.github.com>
2022-07-18 20:04:45 +0530

    Add files via upload

---
+++ studentsmarks.py
@@ -0,0 +1,10 @@
+
+s1=int(input("enter first subject marks"))
+s2=int(input("enter secound subject marks"))
+s3=int(input("enter third subject marks"))
+s4=int(input("enter fourth subject marks"))
+s5=int(input("enter fifth subjest marks"))
+average=(s1+s2+s3+s4+s5)/5
+percentage=((s1+s2+s3+s4+s5)/500)*100
+print(average)
+print(percentage)
